293b71eb507bab1dade30b9de5686316ce05cb12
Deibi Flores <davidflores262@gmail.com>
2024-10-09 13:33:54 -0400

    Full commit

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,11 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_task = Task.objects.filter(is_completed = True)
+    
     context = {
         'tasks': tasks,
+        'completed_task': completed_task,
     }
     return render(request, 'home.html',context)
